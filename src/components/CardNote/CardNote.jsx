132dcc70248edc06cda44a463390df27d2d2966a
ryang <ryan.r.c.339ac@gmail.com>
2021-10-06 20:28:39 -0300

    created the delete button

--- src/components/CardNote/CardNote.jsx
+++ src/components/CardNote/CardNote.jsx
@@ -2,19 +2,32 @@ import React, {Component} from'react';
 import "./card-note.css"
 
 export default class CardNote extends Component{
+    deleteThisTask(){
+        const Delete = (event) =>{
+        const delBtn = event.target
+            const semiParent = delBtn.parentElement.parentElement
+            const deleteParent = semiParent.parentElement
+            deleteParent.remove()
+        }
+        return Delete
+    }
+    
     render(){
         return (
             <section className="CardNote">
                 <header className="CardNote_header">
-                    <h4>
+                    <h2>
                         {this.props.title}
-                    </h4>
+                    </h2>
                 </header>
                 <main>
                     <p>
                         {this.props.text}
                     </p>
                 </main>
+                <footer>
+                    <button className="delBtn" onClick={this.deleteThisTask()}>Delete</button>
+                </footer>
             </section>
         )
     }
