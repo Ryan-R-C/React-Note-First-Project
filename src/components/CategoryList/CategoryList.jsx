7462628f05ab54a026a7ba5dcc56258c8e961f63
ryang <ryan.r.c.339ac@gmail.com>
2021-11-17 09:47:02 -0300

    added classNames to categoryList and created its css

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,13 +1,32 @@
 import React, {Component} from'react';
+import './category-list.css';
 
 export default class CategoryList extends Component{
 render(){
     return(
-        <ul>
-            <li>
-            HELLO
-            </li>
-        </ul>
+        <article className="category-list">
+            <ul  
+            className="category-list__list-father">
+                <li
+                className="category-list__list-father__list-element">
+                    HELLO
+                </li>
+                <li
+                className="category-list__list-father__list-element">
+                    HELLO
+                </li>
+                <li
+                className="category-list__list-father__list-element">
+                    HELLO
+                </li>
+                <li
+                className="category-list__list-father__list-element">
+                    HELLO
+                </li>
+            </ul>
+            <input type="text"
+            className="category-list__input"/>
+        </article>
     )
 }
 }
